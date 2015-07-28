06498f5c3638210ecfc277ca093b27cb5d80bdbb
Dan <dan@miszou.com>
2015-07-28 16:57:01 -0700

    Initial commit

---
+++ SteamShift/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SteamShift
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.cboLeft = new System.Windows.Forms.ComboBox();
+			this.cboRight = new System.Windows.Forms.ComboBox();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.labelFreeSpaceLeft = new System.Windows.Forms.Label();
+			this.listLeft = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.btnMove = new System.Windows.Forms.Button();
+			this.labelFreeSpaceRight = new System.Windows.Forms.Label();
+			this.listRight = new System.Windows.Forms.ListView();
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// cboLeft
+			// 
+			this.cboLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cboLeft.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboLeft.FormattingEnabled = true;
+			this.cboLeft.Location = new System.Drawing.Point(12, 12);
+			this.cboLeft.Name = "cboLeft";
+			this.cboLeft.Size = new System.Drawing.Size(317, 21);
+			this.cboLeft.TabIndex = 0;
+			// 
+			// cboRight
+			// 
+			this.cboRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cboRight.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboRight.FormattingEnabled = true;
+			this.cboRight.Location = new System.Drawing.Point(14, 12);
+			this.cboRight.Name = "cboRight";
+			this.cboRight.Size = new System.Drawing.Size(321, 21);
+			this.cboRight.TabIndex = 1;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.labelFreeSpaceLeft);
+			this.splitContainer1.Panel1.Controls.Add(this.listLeft);
+			this.splitContainer1.Panel1.Controls.Add(this.cboLeft);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+			this.splitContainer1.Size = new System.Drawing.Size(747, 406);
+			this.splitContainer1.SplitterDistance = 343;
+			this.splitContainer1.TabIndex = 4;
+			// 
+			// labelFreeSpaceLeft
+			// 
+			this.labelFreeSpaceLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelFreeSpaceLeft.Location = new System.Drawing.Point(12, 378);
+			this.labelFreeSpaceLeft.Name = "labelFreeSpaceLeft";
+			this.labelFreeSpaceLeft.Size = new System.Drawing.Size(317, 19);
+			this.labelFreeSpaceLeft.TabIndex = 2;
+			this.labelFreeSpaceLeft.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// listLeft
+			// 
+			this.listLeft.AllowDrop = true;
+			this.listLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listLeft.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listLeft.FullRowSelect = true;
+			this.listLeft.HideSelection = false;
+			this.listLeft.Location = new System.Drawing.Point(12, 39);
+			this.listLeft.Name = "listLeft";
+			this.listLeft.Size = new System.Drawing.Size(317, 336);
+			this.listLeft.TabIndex = 1;
+			this.listLeft.UseCompatibleStateImageBehavior = false;
+			this.listLeft.View = System.Windows.Forms.View.Details;
+			this.listLeft.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listLeft_ItemSelectionChanged);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Application";
+			this.columnHeader1.Width = 150;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Size on Disk (Gb)";
+			this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.columnHeader2.Width = 100;
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer2.Name = "splitContainer2";
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.btnMove);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.labelFreeSpaceRight);
+			this.splitContainer2.Panel2.Controls.Add(this.cboRight);
+			this.splitContainer2.Panel2.Controls.Add(this.listRight);
+			this.splitContainer2.Size = new System.Drawing.Size(400, 406);
+			this.splitContainer2.SplitterDistance = 47;
+			this.splitContainer2.TabIndex = 0;
+			// 
+			// btnMove
+			// 
+			this.btnMove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnMove.Enabled = false;
+			this.btnMove.Location = new System.Drawing.Point(3, 12);
+			this.btnMove.Name = "btnMove";
+			this.btnMove.Size = new System.Drawing.Size(41, 23);
+			this.btnMove.TabIndex = 5;
+			this.btnMove.Text = "-->";
+			this.btnMove.UseVisualStyleBackColor = true;
+			this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+			// 
+			// labelFreeSpaceRight
+			// 
+			this.labelFreeSpaceRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelFreeSpaceRight.Location = new System.Drawing.Point(14, 378);
+			this.labelFreeSpaceRight.Name = "labelFreeSpaceRight";
+			this.labelFreeSpaceRight.Size = new System.Drawing.Size(321, 19);
+			this.labelFreeSpaceRight.TabIndex = 3;
+			this.labelFreeSpaceRight.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// listRight
+			// 
+			this.listRight.AllowDrop = true;
+			this.listRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listRight.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+			this.listRight.FullRowSelect = true;
+			this.listRight.HideSelection = false;
+			this.listRight.Location = new System.Drawing.Point(14, 39);
+			this.listRight.Name = "listRight";
+			this.listRight.Size = new System.Drawing.Size(321, 336);
+			this.listRight.TabIndex = 2;
+			this.listRight.UseCompatibleStateImageBehavior = false;
+			this.listRight.View = System.Windows.Forms.View.Details;
+			this.listRight.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listRight_ItemSelectionChanged);
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Application";
+			this.columnHeader3.Width = 150;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Size on Disk (Gb)";
+			this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.columnHeader4.Width = 100;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(747, 406);
+			this.Controls.Add(this.splitContainer1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Steam Library Manager";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cboLeft;
+		private System.Windows.Forms.ComboBox cboRight;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ListView listLeft;
+		private System.Windows.Forms.ListView listRight;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.Label labelFreeSpaceLeft;
+		private System.Windows.Forms.Label labelFreeSpaceRight;
+		private System.Windows.Forms.Button btnMove;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+	}
+}
+
---
+++ SteamShift/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Win32;
+
+using System.Globalization;
+
+namespace SteamShift
+{
+	public partial class Form1 : Form
+	{
+		#region Varaible Declarations and P/Invoke stubs
+		string steamPath = null;
+
+		List<string> Libraries = new List<string>();
+
+		// P/Invoke constants
+		[DllImport( "kernel32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+		[return: MarshalAs( UnmanagedType.Bool )]
+		static extern bool GetDiskFreeSpaceEx( string lpDirectoryName, out ulong lpFreeBytesAvailable, out ulong lpTotalNumberOfBytes, out ulong lpTotalNumberOfFreeBytes );
+
+		#endregion
+
+		protected class ItemData
+		{
+			public string name { get; set; }
+			public string appdir { get; set; }
+			public string vcf { get; set; }
+
+			public override string ToString()
+			{
+				return name;
+			}
+		}
+
+		#region Construction
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load( object sender, EventArgs e )
+		{
+			if( !GetSteamPath() )
+				this.Close();
+
+			ParseVDF();
+			PopulateGUI();
+		}
+		#endregion
+
+		// HKEY_CURRENT_USER\Software\Valve\Steam
+		private bool GetSteamPath()
+		{
+			RegistryKey rk = Registry.CurrentUser;
+			RegistryKey rkSteam = rk.OpenSubKey( "Software\\Valve\\Steam" );
+			steamPath = ((string)rkSteam.GetValue( "SteamPath" ) ).Replace( "/", "\\" );
+
+			if ( steamPath.CompareTo( String.Empty ) == 0 )
+			{
+				MessageBox.Show( "Failed to find your Steam installation" + Environment.NewLine + Environment.NewLine +
+					"Steam is not installed correctly on this computer Reinstall Steam and try again.",
+					"Cannot Locate Steam Folder", MessageBoxButtons.OK, MessageBoxIcon.Error );
+				return false;
+			}
+
+			if ( !ParseVDF() )
+			{
+				MessageBox.Show( "Failed to determine the location of Steam libraries." + Environment.NewLine + Environment.NewLine +
+					"This usually happens if the path to the Steam Installation folder is not set correctly, or your Steam installation " +
+					"is not configured with multiple libraries.",
+					"Cannot Locate Steam Libraries",
+					MessageBoxButtons.OK, MessageBoxIcon.Error );
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool ParseVDF()
+		{
+			string sFile = Path.Combine( steamPath, "SteamApps\\libraryfolders.vdf" );
+			if ( !File.Exists( sFile ) )
+				return false;
+
+			Libraries.Clear();
+			Libraries.Add( ConvertTo_ProperCase( steamPath ) );
+
+			int n = 0;
+			bool bOK = true;
+			foreach ( var line in File.ReadLines( sFile ) )
+			{
+				n++;
+				switch ( line )
+				{
+					case "\"LibraryFolders\"":
+						if ( n != 1 )
+							bOK = false;
+						break;
+
+					case "{":
+						if ( n != 2 )
+							bOK = false;
+						break;
+
+					case "}":
+						break;
+
+					default:
+						{
+							string sLib = line.Substring( line.LastIndexOf( "\t" ) + 1 ).Replace( "\"", "" ).Replace( "\\\\", "\\" );
+							Libraries.Add( sLib );
+						}
+						break;
+				}
+
+				if ( !bOK )
+				{
+					MessageBox.Show( "Unexpected characters on line " + n.ToString() + " in file " + sFile, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+					return false;					
+				}
+			}
+
+			if ( Libraries.Count < 2 )
+			{
+				MessageBox.Show( "Your Steam installation does not appear to contain multiple libraries." + Environment.NewLine + Environment.NewLine +
+					"To use this utility, you must first create a second library in Steam. You can do this do by clicking the \"Steam Library Folders\" button " +
+					"under \"Settings\" | \"Downloads\" in Steam.", "Cannot Locate Additional Steam Libraries", MessageBoxButtons.OK, MessageBoxIcon.Information );
+				return false;
+			}
+
+			return true;
+		}
+
+		private void PopulateGUI()
+		{
+			cboLeft.Items.Clear();
+			cboRight.Items.Clear();
+
+			if ( Libraries.Count > 1 )
+			{
+				for ( int nIndex = 0; nIndex < Libraries.Count; nIndex++ )
+				{
+					if ( nIndex != 1 )
+						cboLeft.Items.Add( Libraries[nIndex] );
+	
+					if (nIndex != 0 )
+						cboRight.Items.Add( Libraries[nIndex] );
+				}
+
+				cboLeft.SelectedIndex = 0;
+				cboRight.SelectedIndex = 0;
+
+				PopulateList( cboLeft.SelectedItem.ToString(), listLeft );
+				PopulateList( cboRight.SelectedItem.ToString(), listRight );
+
+				UpdateFreeSpace( cboLeft.SelectedItem.ToString(), labelFreeSpaceLeft );
+				UpdateFreeSpace( cboRight.SelectedItem.ToString(), labelFreeSpaceRight );
+
+				this.cboLeft.SelectedIndexChanged += new System.EventHandler( this.cboLeft_SelectedIndexChanged );
+				this.cboRight.SelectedIndexChanged += new System.EventHandler( this.cboRight_SelectedIndexChanged );
+			}
+
+			ResizeColumns();
+		}
+
+		private void cboLeft_SelectedIndexChanged( object sender, EventArgs e )
+		{
+			cboRight.Items.Remove( cboLeft.SelectedItem );
+
+			foreach ( string lib in Libraries )
+			{
+				if ( lib != cboLeft.SelectedItem && !cboRight.Items.Contains( lib ) )
+					cboRight.Items.Add( lib );
+			}
+
+			PopulateList( cboLeft.SelectedItem.ToString(),  listLeft );
+			UpdateFreeSpace( cboLeft.SelectedItem.ToString(), labelFreeSpaceLeft );
+		}
+
+		private void cboRight_SelectedIndexChanged( object sender, EventArgs e )
+		{
+			cboLeft.Items.Remove( cboRight.SelectedItem );
+
+			foreach ( string lib in Libraries )
+			{
+				if ( lib != cboRight.SelectedItem && !cboLeft.Items.Contains( lib ) )
+					cboLeft.Items.Add( lib );
+			}
+
+			PopulateList( cboRight.SelectedItem.ToString(), listRight );
+			UpdateFreeSpace( cboRight.SelectedItem.ToString(), labelFreeSpaceRight );
+		}
+
+		private void PopulateList( string sLib, ListView vw )
+		{
+			vw.Items.Clear();
+
+			sLib = Path.Combine( sLib, "steamapps" );
+			if ( !Directory.Exists( sLib ) )
+				return;
+
+			foreach( string sFile in Directory.EnumerateFiles( sLib, "appmanifest_*.acf", System.IO.SearchOption.TopDirectoryOnly ) )
+			{
+				double size = 0;
+				string name = string.Empty;
+				ItemData item = new ItemData();
+				item.vcf = Path.GetFileName( sFile );
+
+				foreach ( var line in File.ReadLines( sFile ) )
+				{
+					if ( line.Contains( "\t\"SizeOnDisk\"" ) )
+					{
+						size = Convert.ToDouble( line.Substring( line.LastIndexOf( "\t" ) + 1 ).Replace( "\"", "" ) );
+						size /= 1073741824; // 1024 * 1024 * 1024
+
+					}
+
+					if ( line.Contains( "\t\"name\"" ) )
+					{
+						item.name = line.Substring( line.LastIndexOf( "\t" ) + 1 ).Replace( "\"", "" );
+					}
+
+					if ( line.Contains( "\t\"installdir\"" ) )
+						item.appdir = line.Substring( line.LastIndexOf( "\t" ) + 1 ).Replace( "\"", "" );
+				}
+
+				ListViewItem lvItem = vw.Items.Add( item.name );
+				lvItem.SubItems.AddRange( new string[] { size.ToString( "F01" ) } );
+				lvItem.Tag = item;
+			}
+		}
+
+		protected void UpdateFreeSpace( string sLib, Label label )
+		{
+			ulong FreeBytesAvailable, TotalNumberOfBytes, TotalNumberOfFreeBytes;
+			bool success = GetDiskFreeSpaceEx( sLib, out FreeBytesAvailable, out TotalNumberOfBytes, out TotalNumberOfFreeBytes );
+
+			label.Text = "Free Space (Gb): " + ( (double)FreeBytesAvailable / 1073741824 ).ToString("F01");
+		}
+
+		private void Form1_Resize( object sender, EventArgs e )
+		{
+			ResizeColumns();
+		}
+
+		private void ResizeColumns()
+		{
+			listLeft.Columns[ 0 ].Width = listLeft.Width - ( listLeft.Columns[ 1 ].Width + 5 );
+			listRight.Columns[ 0 ].Width = listRight.Width - ( listRight.Columns[ 1 ].Width + 5 );
+		}
+
+		private void listLeft_ItemSelectionChanged( object sender, ListViewItemSelectionChangedEventArgs e )
+		{
+			if ( listLeft.SelectedItems.Count > 0 )
+			{
+				btnMove.Enabled = true;
+				btnMove.Text = "-->";
+			}
+			else
+				btnMove.Enabled = false;
+		}
+
+		private void listRight_ItemSelectionChanged( object sender, ListViewItemSelectionChangedEventArgs e )
+		{
+			if ( listRight.SelectedItems.Count > 0 )
+			{
+				btnMove.Enabled = true;
+				btnMove.Text = "<--";
+			}
+			else
+				btnMove.Enabled = false;
+		}
+
+		private void btnMove_Click( object sender, EventArgs e )
+		{
+			using ( Cursor.Current = Cursors.WaitCursor )
+			{
+				// Move left to right
+				if ( btnMove.Text.Contains( ">" ) )
+				{
+					foreach ( ListViewItem lvi in listLeft.SelectedItems )
+					{
+						ItemData item = lvi.Tag as ItemData;
+
+						string srcDir = Path.Combine( cboLeft.SelectedItem.ToString(), "steamapps\\common", item.appdir );
+						string destDir = Path.Combine( cboRight.SelectedItem.ToString(), "steamapps\\common", item.appdir );
+
+						string srcVCF = Path.Combine( cboLeft.SelectedItem.ToString(), "steamapps", item.vcf );
+						string destVCF = Path.Combine( cboRight.SelectedItem.ToString(), "steamapps", item.vcf );
+
+						if ( MoveDirectory( srcDir, destDir ) )
+							File.Move( srcVCF, destVCF );
+					}
+				}
+				// Move right to left
+				else
+				{
+					foreach ( ListViewItem lvi in listRight.SelectedItems )
+					{
+						ItemData item = lvi.Tag as ItemData;
+
+						string srcDir = Path.Combine( cboRight.SelectedItem.ToString(), "steamapps\\common", item.appdir );
+						string destDir = Path.Combine( cboLeft.SelectedItem.ToString(), "steamapps\\common", item.appdir );
+
+						string srcVCF = Path.Combine( cboRight.SelectedItem.ToString(), "steamapps", item.vcf );
+						string destVCF = Path.Combine( cboLeft.SelectedItem.ToString(), "steamapps", item.vcf );
+
+						if ( MoveDirectory( srcDir, destDir ) )
+							File.Move( srcVCF, destVCF );
+					}
+				}
+
+				string sLeft = cboLeft.SelectedItem.ToString();
+				string sRight = cboRight.SelectedItem.ToString();
+
+				PopulateGUI();
+
+				cboLeft.SelectedItem = sLeft;
+				cboRight.SelectedItem = sRight;
+			}
+		}
+
+		protected bool MoveDirectory( string src, string dest )
+		{
+			bool bSuccess = true;
+			try
+			{
+				if ( Path.GetPathRoot( src ).ToLower() == Path.GetPathRoot( dest ).ToLower() )
+					FileSystem.MoveDirectory( src, dest, UIOption.AllDialogs );
+				else
+				{
+					FileSystem.CopyDirectory( src, dest, UIOption.AllDialogs );
+					try
+					{
+						FileSystem.DeleteDirectory( src, DeleteDirectoryOption.DeleteAllContents );
+					}
+					catch ( Exception ex )
+					{
+						MessageBox.Show( "Unable to remove contents of original folder." + Environment.NewLine + Environment.NewLine +
+							"The data was successfully copied to the new location, but an error occurred trying to remove the files from the old location. " +
+							"You can manually delete the contents of the following folder:" + Environment.NewLine + Environment.NewLine + dest,
+							"Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+					}
+				}
+			}
+			catch ( Exception e )
+			{
+				FileSystem.DeleteDirectory( dest, DeleteDirectoryOption.DeleteAllContents );
+				MessageBox.Show( e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error ); 
+				bSuccess = false;
+			}
+			return bSuccess;
+		}
+
+		public static string ConvertTo_ProperCase( string text )
+		{
+			TextInfo myTI = new CultureInfo( "en-US", false ).TextInfo;
+			return myTI.ToTitleCase( text.ToLower() );
+		}
+	}
+}
